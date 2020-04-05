67bce95e2c2758a585c8b23f64b681520c5a87fa
Gleb <trogaev@bk.ru>
2020-04-05 14:55:19 +0700

    update makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,23 @@
 .PHONY: clean
 FLAGS=-Wall -Werror -g3
-bin/chess: $(patsubst src/%.c,build/%.o,$(wildcard src/*.c))
+bin/chess: $(patsubst src/%.c,build/src/%.o,$(wildcard src/*.c))
 	gcc $^ $(FLAGS) -o $@ 
 
-build/%.o: src/%.c
+build/src/%.o: src/%.c
 	gcc -o $@ $(FLAGS) -c -MD $<
 
+test: bin/test
+
+bin/test: $(patsubst test/%.c,build/test/%.o,$(wildcard test/*.c))
+	gcc $^ $(FLAGS) -o $@
+
+build/test/%.o: test/%.c
+	gcc -I thirdparty src -o $@ $(FLAGS) -c -MD $<
+
 clean:
-	rm build/*.o
-	rm build/*.d
+	rm build/src/*.o
+	rm build/src/*.d
+	rm build/test/*.o
+	rm build/test/*.d
 
 include $(wildcard build/*.d)
---
+++ build/test/board_test.d
@@ -0,0 +1,22 @@
+build/test/board_test.o: test/board_test.c \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/stdlib.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/Qt/Tools/mingw730_64/lib/gcc/x86_64-w64-mingw32/7.3.0/include-fixed/limits.h \
+ D:/Qt/Tools/mingw730_64/lib/gcc/x86_64-w64-mingw32/7.3.0/include-fixed/syslimits.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/limits.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/malloc.h \
+ thirdparty/ctest.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/inttypes.h \
+ D:/Qt/Tools/mingw730_64/lib/gcc/x86_64-w64-mingw32/7.3.0/include/stdint.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/stdint.h \
+ D:/Qt/Tools/mingw730_64/lib/gcc/x86_64-w64-mingw32/7.3.0/include/stddef.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/stddef.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/Qt/Tools/mingw730_64/x86_64-w64-mingw32/include/_mingw_print_pop.h
